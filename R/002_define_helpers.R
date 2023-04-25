be3df0aaf7675ea866787c906bf6ff122b8c4f07
edwardlavender <el72@st-andrews.ac.uk>
2023-04-25 13:57:04 +0200

    Add str_range() helper

--- R/002_define_helpers.R
+++ R/002_define_helpers.R
@@ -13,6 +13,17 @@
 ###########################
 #### Define functions
 
+#' @title Define ranges as a string ('a–b')
+
+str_range <- function(x) {
+  if (inherits(x, "character")) {
+    paste0(c(x[1], x[length(x)]), collapse = "–")
+  } else {
+    paste0(c(min(x), max(x)), collapse = "–")
+  }
+}
+
+
 #' @title Parse bins from `cut()`
 #' @description This function parses bins from `cut()`. Bins are defined as the midpoint between each pair of values in the bin. 
 
