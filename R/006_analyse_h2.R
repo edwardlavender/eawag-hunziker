32f8f83eac4686ef62ec14bbdd636a3272228612
edwardlavender <el72@st-andrews.ac.uk>
2023-04-26 14:06:29 +0200

    Add analyse_H2.R

---
+++ R/006_analyse_h2.R
@@ -0,0 +1,275 @@
+#########################
+#########################
+#### analyse_h2.R
+
+#### Aims
+# 1) Analyse H2 (the size of migrants)
+
+#### Prerequisites
+# 1) Process data for analysis (004_process_data.R)
+
+
+#########################
+#########################
+#### Set up
+
+#### Wipe workspace
+rm(list = ls()) 
+try(pacman::p_unload("all"), silent = TRUE) 
+dv::clear() 
+
+#### Essential packages
+library(dv)
+library(dplyr)
+library(mgcv)
+library(DHARMa)
+library(ggeffects)
+library(prettyGraphics)
+library(ggplot2)
+
+#### Load data
+source(here_r("001_define_global_param.R"))
+source(here_r("002_define_helpers.R"))
+fish     <- readRDS(here_data("fish.rds"))
+
+
+#########################
+#########################
+#### Data exploration
+
+#### Define migrants
+migrants <- 
+  fish |> 
+  filter(migration == 1L) |> 
+  select(length, sex, migration_date, migration_yday, yday, stream) 
+# migrants$length <- migrants$length * 10 
+
+#### Check numbers of M/F migrants by stream
+migrants_by_stream <- 
+  migrants |> 
+  group_by(stream, sex) |> 
+  summarise(n = n())
+# Simple statistics
+utils.add::basic_stats(migrants_by_stream$n[migrants_by_stream$sex == "F"])
+utils.add::basic_stats(migrants_by_stream$n[migrants_by_stream$sex == "M"])
+# Barplot of counts
+pp <- par(oma = c(4, 2, 2, 2))
+barplot(migrants_by_stream$n, 
+        names.arg = paste(migrants_by_stream$stream, migrants_by_stream$sex), 
+        las = 2)
+par(pp)
+
+### Check size distributions of migrants by stream
+migrants |> 
+  group_by(stream, sex) |> 
+  summarise(min(length), median(length), mean(length), max(length))
+ggplot(migrants) + 
+  geom_histogram(aes(length)) + 
+  facet_wrap(~stream)
+
+#### Check migration dates of individuals by stream
+# There is evidence of earlier migration in some streams
+ggplot(migrants) + 
+  geom_histogram(aes(migration_yday)) + 
+  facet_wrap(~stream)
+
+#### Check relationships between tagging and migration dates
+plot(migrants$yday, migrants$migration_date)
+cor(migrants$yday, migrants$migration_yday) # ~0.2
+
+
+#########################
+#########################
+#### Implement modelling 
+
+#### Fit models
+# We will fit a model of length ~ sex * time of migration: 
+# * We expect larger individuals to migrate earlier
+# * Smaller individuals may migrate later because predation risk is higher
+# * This relationship may be steeper for females, i.e., 
+# ... while large individuals of both sexes may migrate early
+# ... as time passes, females of a given size may choose to migrate before
+# ... males of the equivalent size 
+# ... i.e., small females risk migration earlier
+
+# mod_1: Initial model
+mod_1 <- gam(length ~ 
+               sex +
+               s(migration_yday, by = sex, bs = c("cc", "fs")) + 
+               s(yday, bs = "cc") + 
+               s(stream, bs = "re"), 
+             knots = list(yday = c(0, 365), 
+                          migration_yday = c(0, 365)),
+             family = gaussian, data = migrants, 
+             method = "REML")
+plot(mod_1, pages = 1, scheme = 1, all.terms = TRUE)
+
+# mod_2: mod_1 without the factor (sex) - smooth (migration day) interaction
+mod_2 <- gam(length ~ 
+               sex +
+               s(migration_yday, bs = "cc") + 
+               s(yday, bs = "cc") + 
+               s(stream, bs = "re"), 
+             knots = list(yday = c(0, 365), 
+                          migration_yday = c(0, 365)),
+             family = gaussian, data = migrants, 
+             method = "REML")
+plot(mod_2, pages = 1, scheme = 1, all.terms = TRUE)
+
+# mod_3: smoothers for each stream
+mod_3 <- gam(length ~ 
+               sex +
+               te(migration_yday, stream, bs = c("cc", "re")) + 
+               s(yday, bs = "cc"),
+             knots = list(yday = c(0, 365), 
+                          migration_yday = c(0, 365)),
+             family = gaussian, data = migrants, 
+             method = "REML")
+plot(mod_3, pages = 1, scheme = 1, all.terms = TRUE)
+
+# mod_4: mod_2 with Gamma likelihood 
+mod_4 <- gam(length ~ 
+               sex +
+               s(migration_yday, bs = "cc") + 
+               s(yday, bs = "cc") + 
+               s(stream, bs = "re"), 
+             knots = list(yday = c(0, 365), 
+                          migration_yday = c(0, 365)),
+             family = Gamma(link = "log"), data = migrants, 
+             method = "REML")
+plot(mod_4, pages = 1, scheme = 1, all.terms = TRUE)
+
+#### Compare models 
+# Check AIC (for models that share the same likelihood)
+# * mod_3 has lowest AIC, but predictions look too wiggly (overfitting)
+# * mod_2 without the interaction is preferable to mod_1 (by ~2.2 AIC)
+# * Residual diagnostics are problematic for mod_2, 
+# ... e.g., evidence of overdispersion in gam.check() and issues in DHARMa resids
+data.frame(mod = 1:3, 
+           aic = c(AIC(mod_1), AIC(mod_2), AIC(mod_3))) |>
+  arrange(aic)
+AIC(mod_1) - AIC(mod_2)
+
+##### Check model summary 
+mod <- mod_4
+summary(mod)
+AIC(mod)
+plot(mod, pages = 1, scheme = 1, all.terms = TRUE)
+
+
+#########################
+#########################
+#### Visualise observations and predictions
+
+#### Quick visuals
+pretty_predictions_1d(mod)
+do.call(gridExtra::grid.arrange, plot(ggpredict(mod), add.data = TRUE))
+
+#### Visualise predictions across all streams
+# TO DO
+
+#### Zoom into stream-specific relationships
+# These plots are motivated by the diagnostic plots (see below)
+# which suggest that Schutzenburnnen is an outlier
+# It is difficult to see here that Schutzenburnnen is an outlier
+# It is true that most migrants are a bit larger than expected
+# across the range of migration days
+# This is shown more clearly below where we plot the effects for each stream
+# The intercept/average value of the smooth is higher in Schützenbrunnen
+# than in the other streams.
+plot(ggpredict(mod, terms = c("sex", "stream")), add.data = TRUE)
+plot(ggpredict(mod, terms = c("migration_yday", "stream", "sex")), add.data = TRUE)
+plot(ggpredict(mod, terms = c("yday", "stream")), add.data = TRUE)
+
+#### Examine the partial effect of stream
+# Define streams 
+streams <- sort(unique(migrants$stream))
+streams <- factor(streams, levels = levels(migrants$stream))
+# Get partial effects 
+# ... since type = "terms" in the code below
+# ... the values of the other variables in newdata don't matter
+nd <- 
+  lapply(streams, function(stream) {
+    d <- model.frame(mod)[1, ]
+    d$stream <- stream
+    d
+  }) |> dplyr::bind_rows()
+nd
+nd$partial <- predict(mod, newdata = nd, type = "terms")[, "s(stream)"]
+# Visualise partial effects
+qn <- qqnorm(nd$partial)
+basicPlotteR::addTextLabels(qn$x, qn$y, labels = nd$stream)
+qqline(nd$partial)
+# We can show these have been correctly computed
+# e.g., via comparison to gratia::draw() output
+# (Hover over the values and compare them to those computed in nd$partial, above)
+if (FALSE) gratia::draw(mod) |> plotly::ggplotly()
+
+#### Examine stream-specific predictions 
+pp <- par(mfrow = c(2, 4), oma = c(3, 3, 1, 1), mar = c(2, 2, 2, 2))
+lapply(seq_len(length(unique(fish$stream))), function(i) {
+  
+  #### Define variables
+  response  <- "length"
+  predictor <- "migration_yday"
+  mframe <- model.frame(mod)
+  stream <- sort(unique(fish$stream))[i]
+  
+  #### Create plot
+  mo <- seq(as.Date("2015-03-01"), as.Date("2015-06-01"), by = "months")
+  jd <- lubridate::yday(mo)
+  mo <- format(mo, "%b")
+  xlim <- c(min(migrants$migration_yday) - 2, max(migrants$migration_yday))
+  
+  axis_ls <- pretty_blank(mframe, predictor, response, 
+                              pretty_axis_args = list(lim = list(x = xlim, y = NULL), 
+                                                      axis = list(x = list(at = jd, labels = mo), 
+                                                                  y = list(NULL)))
+                          )
+  pred <- pred_by_stream(mod, stream, predictor)
+  add_error_envelopes_by_sex(pred, predictor)
+  add_obs_by_sex(mframe[mframe$stream == stream, ], predictor, response)
+  mtext(side = 3, stream, font = 2)
+}) |> invisible()
+
+#### Add axes
+add_axes_labels <- function(cex = 1.25, line = 2, ...) {
+  mtext(side = 1, "Day of migration", cex = cex, line = line, ...)
+  mtext(side = 2, "Standard length (cm)", cex = cex, line = line, ...)
+}
+add_axes_labels(outer = TRUE, line = 1)
+par(pp)
+
+
+#########################
+#########################
+#### Model diagnostics 
+
+#### GAM checks
+# * mod_2: shows evidence of overdispersion
+# * mod_4: also overdispersion, but less strong
+pp <- par(mfrow = c(2, 2))
+gam.check(mod, rep = 1e3)
+par(pp)
+k.check(mod)
+
+#### Simulate DHARMa residuals
+# mod_2: evidence of overdispersion and other issues
+# mod_4: residual diagnostics appear better, 
+# ... although there is still some issue e.g.,
+# ... pattern in residual versus predicted vals
+mframe <- model.frame(mod)
+res    <- simulateResiduals(mod, refit = FALSE, plot = TRUE, re.form = NULL)
+
+#### Check residuals versus predictors
+plotResiduals(res, form = mframe$sex)
+plotResiduals(res, form = mframe[, "migration_yday"])
+plotResiduals(res, form = mframe[, "yday"])
+
+#### Run additional DHARMa checks
+testDispersion(res)
+
+
+#### End of code. 
+#########################
+#########################
