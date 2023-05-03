fc18a98e5bffa1ec730eed02049d5417b85be033
edwardlavender <el72@st-andrews.ac.uk>
2023-05-03 17:16:44 +0200

    Compare the proportion of migratory males versus migratory females

--- R/005_analyse_data.R
+++ R/005_analyse_data.R
@@ -140,6 +140,87 @@ lapply(list(min, median, mean,  max), function(f) {
   f(fish$migration_date[fish$migration == 1L])
 })
 
+#### For migrants, compare the proportion of males versus females
+# Calculate proportions
+prop_ms <- 
+  fish |>
+  filter(migration == 1L) |>
+  mutate(bin = cut(length, seq(min(length), max(length), by = size_class_cm))) |>
+  group_by(bin) |> 
+  summarise(n = n(), 
+            nm = length(which(sex == "M")), 
+            nf = length(which(sex == "F")), 
+            pm = nm/n, 
+            pf = nf/n,) |> 
+  mutate(bin = parse_cut(bin))
+# Define plotting properties
+squash_param <- 20
+prop_ms$cex <- prop_ms$n/squash_param
+tiny <- prop_ms[prop_ms$cex <= 0.5, ]
+# Make plot
+# ... This code is adapted from the plotting routines in analyse_p1.R
+png(here_fig("migrant-prop.png"), 
+    height = 5, width = 7, units = "in", res = 600)
+pp <- par(oma = c(0, 0, 0, 4))
+paa <- list(lim = list(x = lim_length, 
+                       y = c(-0.1, 1.1)),
+            axis = list(x = list(at = at_length), 
+                        y = list(at = seq(0, 1, 0.2))), 
+            control_axis = list(las = TRUE, cex.axis = 1.25)
+            )
+pretty_plot(prop_ms$bin, prop_ms$pm,
+            pretty_axis_args = paa,
+            xlab = "", ylab = "",
+            type = "n")
+lines(prop_ms$bin, prop_ms$pf, 
+      col = scales::alpha(cols["F"], alpha_fit))
+points(prop_ms$bin, prop_ms$pf, 
+       pch = 21, 
+       bg = scales::alpha(cols["F"], alpha_pt), 
+       col = scales::alpha(cols["F"], alpha_pt),
+       cex = prop_ms$n/squash_param)
+points(tiny$bin, tiny$pf, col = "red", lwd = 0.5)
+lines(prop_ms$bin, prop_ms$pm)
+points(prop_ms$bin, prop_ms$pm, 
+       pch = 21, bg = cols["M"], col = cols["M"], 
+       cex = prop_ms$n/squash_param)
+points(tiny$bin, tiny$pm, col = "red", lwd = 0.5)
+pos_f <- which(fish$migration == 1L & fish$sex == "F")
+points(fish$length[pos_f], rep(1, length(pos_f)), 
+    col = scales::alpha(cols["F"], 0.5), cex = 0.5)
+pos_m <- which(fish$migration == 1L & fish$sex == "M")
+points(fish$length[pos_m], rep(0, length(pos_m)), 
+       col = scales::alpha(cols["M"], 0.5), cex = 0.5)
+add_axes_labels <- function(cex = 1.25, line = 2, ...) {
+  mtext(side = 1, "Standard length (cm)", cex = cex, line = line, ...)
+  mtext(side = 2, "Proportion", cex = cex, line = line, ...)
+}
+add_axes_labels()
+px <- par(xpd = NA)
+cex.leg <- 1.1
+legend(25, 1,
+       title = "Sex", title.font = 2,
+       legend = c("F", "M"), 
+       lty = 1, pch = 21, 
+       col = scales::alpha(cols, alpha_pt),
+       pt.bg = scales::alpha(cols, alpha_pt), 
+       bty = "n",
+       cex = cex.leg)
+ns <- c(10, 25, 50)
+legend(25, 0.6,
+       title = "Sample (N)", title.font = 2,
+       legend = ns, 
+       pch = 21, 
+       col = scales::alpha(cols[2], alpha_pt),
+       pt.bg = scales::alpha(cols[2], alpha_pt), 
+       pt.cex = ns/squash_param,
+       y.intersp = 1.4,
+       cex = cex.leg, 
+       bty = "n")
+par(px)
+par(pp)
+dev.off()
+
 
 
 #### End of code.
