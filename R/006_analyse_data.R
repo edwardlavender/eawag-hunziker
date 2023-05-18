7aa764d636a1317cd56f08087f47c32cf3523ac6
edwardlavender <el72@st-andrews.ac.uk>
2023-05-18 09:56:18 +0200

    Define generalised 'size' variable: generalise analyse_data.R

--- R/006_analyse_data.R
+++ R/006_analyse_data.R
@@ -59,7 +59,7 @@ fish |>
             n_migrant = length(which(migration == 1L))) |>
   mutate(pr_migrant = add_lagging_point_zero(round(n_migrant/n, 2), 2)) |>
   arrange(stream) |>
-  tidy_write(here_fig("tables", "stream_summary.txt"))
+  tidy_write(here_fig("summaries", "stream_summary.txt"))
 # Check the number of sections per stream with observations 
 fish |> 
   group_by(stream) |> 
@@ -123,14 +123,14 @@ utils.add::basic_stats(dist_tag$dist_to_ant_from_tag)
 # Tidy table
 dist_tag$dist_to_ant_from_tag  <- round(dist_tag$dist_to_ant_from_tag)
 dist_tag$dist_to_lake_from_tag <- round(dist_tag$dist_to_lake_from_tag)
-dist_tag |> tidy_write(here_fig("tables", "stream_and_section_summary.txt"))
+dist_tag |> tidy_write(here_fig("summaries", "stream_and_section_summary.txt"))
   
 #### Summary table of tagged fish 
 fish |> 
   select(id, date, stream, section, sex, length, migration) |> 
   arrange(date, stream, section, sex, length, migration) |> 
   mutate(id = row_number()) |>
-  tidy_write(here_fig("tables", "tag_data.txt"))
+  tidy_write(here_fig("summaries", "tag_data.txt"))
   
 
 #########################
@@ -148,7 +148,7 @@ tag_sites_dist |>
   facet_wrap(~stream, scales = "free")
 
 #### Size distribution of fish 
-png(here_fig("fish-size-distribution.png"), 
+png(here_fig("fish-length-distribution.png"), 
     height = 4, width = 5, units = "in", res = 600)
 bw <- 0.2
 bks <- seq(plyr::round_any(min(fish$length), bw, floor), 
@@ -180,37 +180,53 @@ plot(fish$yday, fish$length)
 cor(fish$yday, fish$length)
 
 #### Calculate the observed proportion of migrants by sex and size (ss)
-size_class_cm <- 1.5
+size_class <- list(length = 1.5,
+                   mass = 1.5)
+size_vars <- c("length", "mass")
 prop_ss <- 
-  fish |>
-  # group_by(sex) |>
-  mutate(bin = cut(length, seq(min(length), max(length), by = size_class_cm))) |>
-  # ungroup() |>
-  group_by(sex, bin) |>
-  summarise(pr = length(which(migration == "1"))/n(), 
-            n = n()) |>
-  mutate(length = parse_cut(bin), 
-         col = as.character(scales::alpha(cols, alpha_pt)[as.character(sex)])) |> 
-  select(sex, n, bin, length, pr, col) |>
-  filter(!is.na(bin)) |> 
-  as.data.frame()
+  lapply(size_vars, function(size_var) {
+    fish$size <- fish[, size_var]
+    fish |>
+      # group_by(sex) |>
+      mutate(bin = cut(size, seq(min(size, na.rm = TRUE), 
+                                 max(size, na.rm = TRUE), 
+                                 by = size_class[[size_var]]))
+             ) |>
+      # ungroup() |>
+      group_by(sex, bin) |>
+      summarise(pr = length(which(migration == "1"))/n(), 
+                n = n()) |>
+      mutate(size = parse_cut(bin), 
+             col = as.character(scales::alpha(cols, alpha_pt)[as.character(sex)])) |> 
+      select(sex, n, bin, size, pr, col) |>
+      filter(!is.na(bin)) |> 
+      as.data.frame()
+  })
+names(prop_ss) <- size_vars
 saveRDS(prop_ss, here_data("prop_ss.rds"))
 
 #### Calculate the observed proportion of migrants by sex, size and stream (sss)
 # This code is the same as above but stream is included in the grouping structures
 prop_sss <- 
-  fish |>
-  group_by(stream, sex) |>
-  mutate(bin = cut(length, seq(min(length), max(length), by = size_class_cm))) |>
-  ungroup() |>
-  group_by(stream, sex, bin) |>
-  summarise(pr = length(which(migration == "1"))/n(), 
-            n = n()) |>
-  mutate(length = parse_cut(bin), 
-         col = as.character(scales::alpha(cols, alpha_pt)[as.character(sex)])) |> 
-  select(stream, sex, n, bin, length, pr, col) |>
-  filter(!is.na(bin)) |> 
-  as.data.frame()
+  lapply(size_vars, function(size_var) {
+    fish$size <- fish[, size_var]
+    fish |>
+      group_by(stream, sex) |>
+      mutate(bin = cut(size, seq(min(size, na.rm = TRUE), 
+                                 max(size, na.rm = TRUE), 
+                                 by = size_class[[size_var]]))
+             ) |>
+      ungroup() |>
+      group_by(stream, sex, bin) |>
+      summarise(pr = length(which(migration == "1"))/n(), 
+                n = n()) |>
+      mutate(size = parse_cut(bin), 
+             col = as.character(scales::alpha(cols, alpha_pt)[as.character(sex)])) |> 
+      select(stream, sex, n, bin, size, pr, col) |>
+      filter(!is.na(bin)) |> 
+      as.data.frame()
+  })
+names(prop_sss) <- size_vars
 saveRDS(prop_sss, here_data("prop_sss.rds"))
 
 #### For migrant individuals, check sex, sizes and the timing of migration
@@ -235,85 +251,88 @@ lapply(list(min, median, mean,  max), function(f) {
 
 #### For migrants, compare the proportion of males versus females
 # Calculate proportions
-prop_ms <- 
-  fish |>
-  filter(migration == 1L) |>
-  mutate(bin = cut(length, seq(min(length), max(length), by = size_class_cm))) |>
-  group_by(bin) |> 
-  summarise(n = n(), 
-            nm = length(which(sex == "M")), 
-            nf = length(which(sex == "F")), 
-            pm = nm/n, 
-            pf = nf/n,) |> 
-  mutate(bin = parse_cut(bin))
-# Define plotting properties
-squash_param <- 20
-prop_ms$cex <- prop_ms$n/squash_param
-tiny <- prop_ms[prop_ms$cex <= 0.5, ]
-# Make plot
-# ... This code is adapted from the plotting routines in analyse_p1.R
-png(here_fig("migrant-prop.png"), 
-    height = 5, width = 7, units = "in", res = 600)
-pp <- par(oma = c(0, 0, 0, 4))
-paa <- list(lim = list(x = lim_length, 
-                       y = c(-0.1, 1.1)),
-            axis = list(x = list(at = at_length), 
-                        y = list(at = seq(0, 1, 0.2))), 
-            control_axis = list(las = TRUE, cex.axis = 1.25)
-            )
-pretty_plot(prop_ms$bin, prop_ms$pm,
-            pretty_axis_args = paa,
-            xlab = "", ylab = "",
-            type = "n")
-lines(prop_ms$bin, prop_ms$pf, 
-      col = scales::alpha(cols["F"], alpha_fit))
-points(prop_ms$bin, prop_ms$pf, 
-       pch = 21, 
-       bg = scales::alpha(cols["F"], alpha_pt), 
-       col = scales::alpha(cols["F"], alpha_pt),
-       cex = prop_ms$n/squash_param)
-points(tiny$bin, tiny$pf, col = "red", lwd = 0.5)
-lines(prop_ms$bin, prop_ms$pm)
-points(prop_ms$bin, prop_ms$pm, 
-       pch = 21, bg = cols["M"], col = cols["M"], 
-       cex = prop_ms$n/squash_param)
-points(tiny$bin, tiny$pm, col = "red", lwd = 0.5)
-pos_f <- which(fish$migration == 1L & fish$sex == "F")
-points(fish$length[pos_f], rep(1, length(pos_f)), 
-    col = scales::alpha(cols["F"], 0.5), cex = 0.5)
-pos_m <- which(fish$migration == 1L & fish$sex == "M")
-points(fish$length[pos_m], rep(0, length(pos_m)), 
-       col = scales::alpha(cols["M"], 0.5), cex = 0.5)
-add_axes_labels <- function(cex = 1.25, line = 2, ...) {
-  mtext(side = 1, "Standard length (cm)", cex = cex, line = line, ...)
-  mtext(side = 2, "Proportion", cex = cex, line = line, ...)
-}
-add_axes_labels()
-px <- par(xpd = NA)
-cex.leg <- 1.1
-legend(25, 1,
-       title = "Sex", title.font = 2,
-       legend = c("F", "M"), 
-       lty = 1, pch = 21, 
-       col = scales::alpha(cols, alpha_pt),
-       pt.bg = scales::alpha(cols, alpha_pt), 
-       bty = "n",
-       cex = cex.leg)
-ns <- c(10, 25, 50)
-legend(25, 0.6,
-       title = "Sample (N)", title.font = 2,
-       legend = ns, 
-       pch = 21, 
-       col = scales::alpha(cols[2], alpha_pt),
-       pt.bg = scales::alpha(cols[2], alpha_pt), 
-       pt.cex = ns/squash_param,
-       y.intersp = 1.4,
-       cex = cex.leg, 
-       bty = "n")
-par(px)
-par(pp)
-dev.off()
-
+lapply(c("length", "mass"), function(size_var) {
+  fish$size <- fish[, size_var]
+  prop_ms <- 
+    fish |>
+    filter(migration == 1L) |>
+    mutate(bin = cut(size, seq(min(size), max(size), by = size_class[[size_var]]))) |>
+    group_by(bin) |> 
+    summarise(n = n(), 
+              nm = length(which(sex == "M")), 
+              nf = length(which(sex == "F")), 
+              pm = nm/n, 
+              pf = nf/n,) |> 
+    mutate(bin = parse_cut(bin))
+  # Define plotting properties
+  squash_param <- 20
+  prop_ms$cex <- prop_ms$n/squash_param
+  tiny <- prop_ms[prop_ms$cex <= 0.5, ]
+  # Make plot
+  # ... This code is adapted from the plotting routines in analyse_p1.R
+  png(here_fig("relationships", "fig", glue::glue("by-{size_var}"), "migrant-prop.png"), 
+      height = 5, width = 7, units = "in", res = 600)
+  pp <- par(oma = c(0, 0, 0, 4))
+  paa <- list(lim = list(x = lim_size[[size_var]], 
+                         y = c(-0.1, 1.1)),
+              axis = list(x = list(at = at_size[[size_var]]), 
+                          y = list(at = seq(0, 1, 0.2))), 
+              control_axis = list(las = TRUE, cex.axis = 1.25)
+  )
+  pretty_plot(prop_ms$bin, prop_ms$pm,
+              pretty_axis_args = paa,
+              xlab = "", ylab = "",
+              type = "n")
+  lines(prop_ms$bin, prop_ms$pf, 
+        col = scales::alpha(cols["F"], alpha_fit))
+  points(prop_ms$bin, prop_ms$pf, 
+         pch = 21, 
+         bg = scales::alpha(cols["F"], alpha_pt), 
+         col = scales::alpha(cols["F"], alpha_pt),
+         cex = prop_ms$n/squash_param)
+  points(tiny$bin, tiny$pf, col = "red", lwd = 0.5)
+  lines(prop_ms$bin, prop_ms$pm)
+  points(prop_ms$bin, prop_ms$pm, 
+         pch = 21, bg = cols["M"], col = cols["M"], 
+         cex = prop_ms$n/squash_param)
+  points(tiny$bin, tiny$pm, col = "red", lwd = 0.5)
+  pos_f <- which(fish$migration == 1L & fish$sex == "F")
+  points(fish$length[pos_f], rep(1, length(pos_f)), 
+         col = scales::alpha(cols["F"], 0.5), cex = 0.5)
+  pos_m <- which(fish$migration == 1L & fish$sex == "M")
+  points(fish$length[pos_m], rep(0, length(pos_m)), 
+         col = scales::alpha(cols["M"], 0.5), cex = 0.5)
+  add_axes_labels <- function(cex = 1.25, line = 2, ...) {
+    mtext(side = 1, "Standard length (cm)", cex = cex, line = line, ...)
+    mtext(side = 2, "Proportion", cex = cex, line = line, ...)
+  }
+  add_axes_labels()
+  px <- par(xpd = NA)
+  cex.leg <- 1.1
+  legend(25, 1,
+         title = "Sex", title.font = 2,
+         legend = c("F", "M"), 
+         lty = 1, pch = 21, 
+         col = scales::alpha(cols, alpha_pt),
+         pt.bg = scales::alpha(cols, alpha_pt), 
+         bty = "n",
+         cex = cex.leg)
+  ns <- c(10, 25, 50)
+  legend(25, 0.6,
+         title = "Sample (N)", title.font = 2,
+         legend = ns, 
+         pch = 21, 
+         col = scales::alpha(cols[2], alpha_pt),
+         pt.bg = scales::alpha(cols[2], alpha_pt), 
+         pt.cex = ns/squash_param,
+         y.intersp = 1.4,
+         cex = cex.leg, 
+         bty = "n")
+  par(px)
+  par(pp)
+  dev.off()
+  
+})
 
 
 #### End of code.
